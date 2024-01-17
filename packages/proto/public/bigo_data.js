1d2845fee216beda946573aa13fd48854c5e244a
upluke <lukeforadream@gmail.com>
2024-01-16 19:44:46 -0800

    added data

---
+++ packages/proto/public/bigo_data.js
@@ -0,0 +1,16 @@
+const bigOData = [
+  {
+    title: "O(1)",
+    definition:
+      "It means that it takes a constant time, and it does not depends on the data that it is processing...",
+    content:
+      "It takes a constant time to run an algorithm, regardless of the size of the input...",
+  },
+  {
+    title: "O(n)",
+    definition:
+      "It indicates the order of growth of some quantity as a function of n or the limiting behavior of a function...",
+    content:
+      "It is an approximation of the duration of the algorithm given N input elements...",
+  },
+];
---
+++ packages/proto/public/dsalgo_data.js
@@ -0,0 +1,48 @@
+const dsAlgoData = {
+  sorting: [
+    {
+      insertion_sort: {
+        title: "Insertion Sort",
+        definition: "Insertion sort is a simple sorting algorithm...",
+        content:
+          "Insertion sort is used when the number of elements is small...",
+      },
+    },
+    {
+      quick_sort: {
+        title: "Quick Sort",
+        definition:
+          "Quicksort is an efficient, general-purpose sorting algorithm...",
+        content:
+          "QuickSort is a sorting algorithm based on the Divide and Conquer algorithm...",
+      },
+    },
+    {
+      merge_sort: {
+        title: "Merge Sort",
+        definition:
+          "Merge sort is one of the most efficient sorting algorithms...",
+        content: "Merge sort is based on the divide-and-conquer strategy...",
+      },
+    },
+  ],
+  linkedList: [
+    {
+      singly_linked_list: {
+        title: "Singly Linked List",
+        definition: "A singly linked list is a linear data structure...",
+        content:
+          "It can be traversed in only one direction from head to the last node.",
+      },
+    },
+    {
+      singly_linked_list: {
+        title: "Doubly Linked List",
+        definition:
+          "A doubly linked list is a linked data structure that consists of a set of sequentially linked records called nodes. ...",
+        content: "It can be traversed in two directions...",
+      },
+    },
+    // Add other linked list types
+  ],
+};
