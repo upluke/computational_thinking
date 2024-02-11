8976128c6346372c95e9c17ea80907e2e8b7f151
upluke <lukeforadream@gmail.com>
2024-02-11 15:50:12 -0800

    fised profile drop down

--- packages/lit-frontend/components/user-panel.ts
+++ packages/lit-frontend/components/user-panel.ts
@@ -1,4 +1,3 @@
-import { Router } from "@vaadin/router";
 import { customElement, property } from "lit/decorators.js";
 import { html, css, LitElement } from "lit";
 
@@ -11,8 +10,9 @@ export class UserPanel extends LitElement {
 
     .user-panel {
       background-color: var(--background-color);
-      padding: 16px;
-      border-radius: 8px;
+
+      /* padding: 16px; */
+      /* border-radius: 8px; */
     }
 
     .user-avatar {
@@ -23,28 +23,9 @@ export class UserPanel extends LitElement {
     }
 
     .user-info {
-      display: flex;
-      align-items: center;
+      /* display: flex; */
+      /* align-items: center; */
       margin-bottom: 12px;
-    }
-
-    .user-name {
-      font-weight: bold;
-    }
-
-    .user-link {
-      color: var(--hero-background-color);
-      text-decoration: none;
-      margin-bottom: 8px;
-      display: block;
-    }
-
-    .sign-out {
-      color: green;
-      cursor: pointer;
-      text-decoration: none;
-      margin-top: 16px;
-      display: block;
     }
   `;
 
@@ -57,21 +38,8 @@ export class UserPanel extends LitElement {
             src="https://www.svgrepo.com/show/382112/female-avatar-girl-face-woman-user-8.svg"
             alt="User Avatar"
           />
-          <div>
-            <span class="user-name">Mike</span><br />
-            <a class="user-link" href="/#">About</a>
-            <a class="user-link" @click=${this.navigateToProfile}>Profile</a>
-          </div>
         </div>
-        <a class="sign-out" @click=${this.signOut}>Sign out</a>
       </div>
     `;
-  }
-
-  signOut() {
-    console.log("Signing out...");
-  }
-  navigateToProfile() {
-    Router.go("/user-profile");
   }
 }
