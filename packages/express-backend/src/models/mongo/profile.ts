20fa68a2f57746adab7aba588b727369fa4ec1fe
upluke <lukeforadream@gmail.com>
2024-02-19 22:23:15 -0800

    added profile model and schema

---
+++ packages/express-backend/src/models/mongo/profile.ts
@@ -0,0 +1,18 @@
+// src/models/mongo/profile.ts
+import { Schema, Model, Document, model } from "mongoose";
+import { Profile } from "../profile";
+
+const profileSchema = new Schema<Profile>(
+  {
+    userid: { type: String, required: true, trim: true },
+    name: { type: String, required: true, trim: true },
+    nickname: { type: String, trim: true },
+    city: { type: String, trim: true },
+    contentProgress: [String],
+  },
+  { collection: "user_profiles" }
+);
+
+const ProfileModel = model<Profile>("Profile", profileSchema);
+
+export default ProfileModel;
